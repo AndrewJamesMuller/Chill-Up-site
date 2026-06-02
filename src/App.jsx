3873c7ee870d03a6006274d2250a67131d48b2de
Дрибуленда Андрій <andriyshef10@gmail.com>
2026-06-02 18:24:58 +0300

    allready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import Home from './pages/Home'
+import ProfCatalog from './pages/ProfCatalog'
 import "./App.css"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="ProfCatalog" element={<ProfCatalog />} />
         {/* <Route path="about" element={<About />} /> */}
       </Route>
     </Routes>
---
+++ src/pages/ProfCatalog.jsx
@@ -0,0 +1,8 @@
+import "./profCatalog.css"
+export default function ProfCatalog() {
+    return (
+        <div className="prof-catalog-container">
+            <h1 className="prof-catalog-title">Професії</h1>
+        </div>
+    )
+}
