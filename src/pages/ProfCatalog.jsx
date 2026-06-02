5ce088805c488de885056f3a5a448708f1e2c30a
Дрибуленда Андрій <andriyshef10@gmail.com>
2026-06-02 19:53:39 +0300

    catalog almost ready

--- src/pages/ProfCatalog.jsx
+++ src/pages/ProfCatalog.jsx
@@ -11,6 +11,13 @@ export default function ProfCatalog() {
                     <Link to="/ProfCatalog/law" className="link">
                         <CatalogBlockLeft img="/public/law.svg" text="Право" />
                     </Link>
+                    <Link to="/ProfCatalog/builder" className="link">
+                        <CatalogBlockLeft img="/public/builder1.svg" text="Будівництво" />
+                    </Link>
+                    <Link to="/ProfCatalog/sports" className="link">
+                        <CatalogBlockLeft img="/public/sports1.svg" text="Спорт" />
+                    </Link>
+                    
                 </div>
                 <div className="container-right"></div>
             </div>
