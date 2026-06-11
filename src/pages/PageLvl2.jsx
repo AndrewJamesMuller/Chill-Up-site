9c5dbe8ae29564f4b222f2245c54c4893a27e6cb
Дрибуленда Андрій <andriyshef10@gmail.com>
2026-06-11 18:59:36 +0300

    page2nd ready

--- src/pages/PageLvl2.jsx
+++ src/pages/PageLvl2.jsx
@@ -1,3 +1,34 @@
+import "./pageLvl2.css"
+import { useParams, Link } from "react-router-dom"
+import { pages } from "../data"
 export default function PageLvl2(){
-    
+    const {slug} = useParams()
+    const category = pages.find(
+        p => p.slug === slug
+    )
+     if (!category) {
+         return <h1>404</h1>;
+    }
+    const children = pages.filter(
+        p => category.children?.includes(p.slug)
+    )
+    return( 
+        <div className="mainLvl2">
+            <h1 className="title">
+                {category.title}
+            </h1>
+            <div className="contentBlock">
+                <img src={category.img} alt={category.title} className="image" />
+                <div className="textBlock">
+                    <p className="text">{category.mainText}</p>
+                </div>
+            </div>
+            <h2 className="title2">
+                Напрями:
+            </h2>
+            <div className="professionsList">
+                
+            </div>
+        </div>
+    )
 }
