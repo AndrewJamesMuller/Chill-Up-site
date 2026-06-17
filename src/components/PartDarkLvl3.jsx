7df23ba3937aca8d409467a2010205ad0c3a961a
Дрибуленда Андрій <andriyshef10@gmail.com>
2026-06-17 22:16:31 +0300

    page level 3 is ready

--- src/components/PartDarkLvl3.jsx
+++ src/components/PartDarkLvl3.jsx
@@ -1,3 +1,17 @@
-export default function PartDarkLvl3(){
-    
+import "../pages/pageLvl3.css"
+export default function PartDarkLvl3({array, title}){
+   return(
+     <section className="partDark">
+            <div className="titleBlockDark">
+                <h2 className="titleDark">{title}</h2>
+                </div>
+                <ul className="listDark">
+                        {array.map(item=>(
+                            <li className="elementDark" key={item}>
+                                {item}
+                            </li>
+                        ))}
+                </ul>
+        </section>
+   )
 }
